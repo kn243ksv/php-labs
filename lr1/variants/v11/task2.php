bfcdda445675e285d830b0ec459d26f9addd8b2c
kn243ksv <kn243_ksv@student.ztu.edu.ua>
2026-02-16 19:13:34 +0200

    commit

---
+++ lr1/variants/v11/task2.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Завдання 1: Форматований текст
+ *
+ * Вірш про художника з форматуванням: <b>, <i>, margin-left
+ */
+require_once __DIR__ . '/layout.php';
+
+ob_start();
+?>
+<div class="poem">
+    <?php
+    echo "<p style='margin-left: 20px;'>Мій <b>місто</b> прокидається вранці,</p>";
+    echo "<p style='margin-left: 20px;'>Трамваї дзвенять у <i>тумані</i>,</p>";
+    echo "<p style='margin-left: 20px;'>Вулиці повні людей і машин,</p>";
+    echo "<p style='margin-left: 20px;'>І кожен кудись поспішає один.</p>";
+    ?>
+</div>
+<?php
+$content = ob_get_clean();
+
+renderVariantLayout($content, 'Завдання 1', 'task2-body');
